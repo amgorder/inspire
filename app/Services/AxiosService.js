d10d4f06e6e05d2864120b28ee9223cf19f41053
Aaron Gorder <aarongorder@gmail.com>
2021-02-28 15:50:58 -0700

    ignor error msg axios

--- app/Services/AxiosService.js
+++ app/Services/AxiosService.js
@@ -1,8 +1,10 @@
+// @ts-ignore
 export const sandboxApi = axios.create({
     baseURL: 'https://bcw-sandbox.herokuapp.com/api/',
     timeout: 3000
 })
 
+// @ts-ignore
 export const todoApi = axios.create({
     baseURL: "https://bcw-sandbox.herokuapp.com/api/gorder/todo/",
     timeout: 3000
